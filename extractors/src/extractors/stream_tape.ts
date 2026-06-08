0bfff60bd39cc9fca57d0a8c1197e3982f452633
TechSolanki <mradulpratapsolanki@gmail.com>
2026-06-08 09:42:08 +0530

    Update stream_tape.ts

--- extractors/src/extractors/stream_tape.ts
+++ extractors/src/extractors/stream_tape.ts
@@ -9,10 +9,12 @@ export class StreamTape extends ExtractorApi {
   async getUrl(url: string, referer?: string): Promise<IExtractorLink[]> {
     // Header ko simple object ki tarah pass karein
     const res = await http_get(url, {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-    });
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    } //
+});
 
-    if (res.status !== 200) return [];
+  if (res.status !== 200) return [];
 
     // Streamtape ka dynamic link dhundhne ke liye Regex
     // Ye code 'robotlink' ke andar ka JS logic pakdega
